0dd5168df0b2a59357bc102cc93a6f90939f7f5f
cs <siwon.cezary@gmail.com>
2023-04-01 13:04:33 +0200

    StreamDemo

---
+++ src/main/java/pl/sda/pol141/day4/io/StreamDemo.java
@@ -0,0 +1,19 @@
+package pl.sda.pol141.day4.io;
+
+import java.io.*;
+
+public class StreamDemo {
+    public static void main(String[] args) throws IOException {
+        OutputStream stream = new PrintStream(System.out);
+        stream.write("Hello World!".getBytes());
+        stream = new FileOutputStream("c:\\data\\console.txt");
+        stream.write("Hello World!".getBytes());
+        byte[] buffer = new byte[1024];
+        stream = new ByteArrayOutputStream();
+        stream.write("Hello Wordl!".getBytes());
+
+        InputStream inputStream = new FileInputStream("c:\\data\\console.txt");
+        final int read = inputStream.read();
+        System.out.println(read);
+    }
+}
